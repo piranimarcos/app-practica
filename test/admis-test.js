1b0a2e1e780600aede5fb1ff155d3e58d72d04fa
Marcos Pirani <marcos.pir94@gmail.com>
2015-10-28 19:52:39 -0300

    creacion de modelo admin y testing del mismo

--- test/admis-test.js
+++ test/admis-test.js
@@ -1,14 +1,3 @@
-/*var Admin = require('../models/admins');
-var mongoose = require('mongoose');
-
-mongoose.connect('mongodb://localhost/crudtest');
-
-var a = new Admin({ email:"admin@admin.com", password: "123456" });
-a.save(function(err, doc){
-    console.log(err, doc);    
-});*/
-
-
 var Admin = require('../models/admins'),
 	mongoose = require('mongoose');
 
